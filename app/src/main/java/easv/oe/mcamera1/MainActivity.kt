4e7bbfb1327d182f389afef785997e780426111f
oleeriksen <oe@easv.dk>
2021-03-10 21:41:00 +0100

    Initial commit

---
+++ app/src/main/java/easv/oe/mcamera1/MainActivity.kt
@@ -0,0 +1,153 @@
+package easv.oe.mcamera1
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "xyz"
+    private val PERMISSION_REQUEST_CODE = 1
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE = 101
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP = 102
+
+    var mFile: File? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
+    }
+
+
+    fun onTakeByFile(view: View) {
+        mFile = getOutputMediaFile() // create a file to save the image
+
+        if (mFile == null) {
+            Toast.makeText(this, "Could not create file...", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // create Intent to take a picture
+
+        // create Intent to take a picture
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        val applicationId = "easv.oe.mcamera1"
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(
+                this,
+                "${applicationId}.provider",  //use your app signature + ".provider"
+                mFile!!))
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE)
+        } else Log.d(TAG, "camera app could NOT be started")
+
+    }
+
+    fun onTakeByBitmap(view: View) {}
+
+
+    /**
+     * Checks if the app has the required permissions, and prompts the user with the ones missing.
+     */
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
+        val permissions = mutableListOf<String>()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.CAMERA)
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        //    permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        if (permissions.size > 0)
+            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PERMISSION_REQUEST_CODE)
+    }
+
+
+    private fun getOutputMediaFile(): File? {
+        // in an emulated device you can see the external files in /sdcard/Android/data/<your app>.
+          val mediaStorageDir = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Camera01")
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory")
+                return null
+            }
+        }
+
+        // Create a media file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val postfix = "jpg"
+        val prefix = "IMG"
+        return File(mediaStorageDir.path +
+                File.separator + prefix +
+                "_" + timeStamp + "." + postfix)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val mImage = findViewById<ImageView>(R.id.imgView)
+        val mFilename = findViewById<TextView>(R.id.txtFileName)
+        when (requestCode) {
+
+            CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE ->
+                if (resultCode == RESULT_OK) {
+                        mImage.setImageURI(Uri.fromFile(mFile))
+                        mImage.setBackgroundColor(Color.RED)
+                      //mImage.setRotation(90);
+                    mFilename.text = mFile?.absolutePath + " - size = " + mFile?.length()
+                 } else handleOther(resultCode)
+
+            CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP ->
+                if (resultCode == RESULT_OK) {
+                        val extras = data!!.extras
+                        val imageBitmap = extras!!["data"] as Bitmap?
+                        Log.d(TAG, "Size of bitmap = " + imageBitmap!!.byteCount)
+                        mImage.setImageBitmap(imageBitmap)
+                        mImage.setLayoutParams(RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+                        mImage.setBackgroundColor(Color.RED)
+                        mFilename.setText("bitmap - size = " + imageBitmap.byteCount)
+                 } else handleOther(resultCode)
+        }
+    }
+
+    private fun handleOther(resultCode: Int) {
+        if (resultCode == RESULT_CANCELED)
+            Toast.makeText(this, "Canceled...", Toast.LENGTH_LONG).show()
+        else Toast.makeText(this, "Picture NOT taken - unknown error...", Toast.LENGTH_LONG).show()
+    }
+
+
+    /*private fun showPictureTaken(f: File) {
+        mImage.setImageURI(Uri.fromFile(f))
+        mImage.setBackgroundColor(Color.RED)
+        //mImage.setRotation(90);
+        mFilename.setText(f.absolutePath + " - size = " + f.length())
+    }*/
+
+
+}
